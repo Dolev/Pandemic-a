6c9414422f23d23c9ed3f7f39f36096fbecadb02
Dolev .H <33119992+Dolev@users.noreply.github.com>
2021-05-05 20:42:19 +0300

    Update Board.cpp

--- Board.cpp
+++ Board.cpp
@@ -1,49 +1,39 @@
 #include "Board.hpp"
 #include "City.hpp"
 #include <fstream>
-#include <stdexcept>
-#include <iostream>  
+using namespace std;
+using namespace pandemic;
 
 namespace pandemic{
-    map<City, int> Board::City_Cubes;
-    
+
     Board::Board(){
         City_Cubes[City::Kinshasa]=0;
         City_Cubes[City::MexicoCity]=0;
         City_Cubes[City::HoChiMinhCity]=0;
         City_Cubes[City::Chicago]=0;
+        City_Cubes[City::Bogota]=0;
+        City_Cubes[City::Atlanta]=0;
+        City_Cubes[City::Essen]=0;
+        
     }
 
-     int& Board::operator[](const City& CityId){
+    int& Board::operator[](const City& CityId){
         if(City_Cubes.find(CityId)==City_Cubes.end()){
-            throw "This City isn't in the board ";
+            throw "This City isnt in options";
         }
-        return City_Cubes.at(CityId);//get the num in the map by key  CityId
+        
+        return City_Cubes.at(CityId);
+        
     }
     ostream& operator<<(ostream& Output, const Board& board){
         return Output;
     }
+
     bool Board::is_clean(){
-       //cout << " Initially, City_Cubes.empty(): " << City_Cubes.empty() << "\n";   //Should Be 48
-       bool flag=false;
-       if(City_Cubes.empty()){//empty map
-            flag=true;
-       }
-       return flag;       
+        return true;
     }
 
-     Board Board::remove_cures(){
-          Board temp;
-        return temp;
-    }
-}
-
-/*
-void print_map(std::unordered_map<K, V> const &m)
-{
-    for (auto it = m.cbegin(); it != m.cend(); ++it) {
-        if(Num_of_Cubes[it]==0){//if all cubes clean
-        }
-    }
-}
-*/
+    void Board::remove_cures(){}
+  
+    
+}  
